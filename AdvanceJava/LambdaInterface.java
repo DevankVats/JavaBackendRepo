bb38c4e6691795b9b568446297694a2a61138310
DevankVats <vatsdevank2610@gmail .com>
2026-01-07 14:59:47 +0530

    Lambda Expression

--- AdvanceJava/LambdaInterface.java
+++ AdvanceJava/LambdaInterface.java
@@ -1,3 +1,15 @@
+interface Addition{
+  int addition(int a,int b);
+}
+
 public class LambdaInterface {
+  public static void main(String[] args) {
+    Addition add=(a,b)->a+b;
+
+    int result=add.addition(5,7);
+    System.out.println("the sum is: "+result);
+
+    
+  }
   
 }
