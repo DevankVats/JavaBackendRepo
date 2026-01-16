93fb54ea3444828e38e954ead4dab0e04af811b5
DevankVats <vatsdevank2610@gmail .com>
2026-01-16 10:17:45 +0530

    Threading and Concurrency

---
+++ AdvanceJava/ImplementingRunnableInterface.java
@@ -0,0 +1,31 @@
+public class ImplementingRunnableInterface {
+  public static void main(String[] args) {
+    Runnable obj1=()->{
+      for(int i=0;i<5;i++){
+         System.out.println("hi");
+        try{
+          Thread.sleep(10);
+        }catch(InterruptedException e){
+          e.printStackTrace();
+        }
+      }
+    };
+    Runnable obj2=()->{
+      for(int i=0;i<5;i++){
+        System.out.println("hello");
+        try{
+          Thread.sleep(10);
+        }catch(InterruptedException e){
+          e.printStackTrace();
+        }
+      }
+    };
+
+    Thread t1=new Thread(obj1);
+    Thread t2=new Thread(obj2);
+
+    t1.start();
+    t2.start();
+
+  }
+}
---
+++ AdvanceJava/ImplementingThreads.java
@@ -0,0 +1,50 @@
+class A extends Thread{
+ public  void run(){
+    for(int i=0;i<20;i++){
+      System.out.println("Hello");
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
+}
+class B extends Thread{
+  public void run(){
+    for(int i=0;i<20;i++){
+      System.out.println("world!");
+      try{
+        Thread.sleep(10);
+      }catch(InterruptedException e){
+        e.printStackTrace();
+      }
+    }
+  }
+}
+
+
+
+
+public class ImplementingThreads{
+  public static void main(String[] args) {
+    A obj1=new A();
+    B obj2=new B();
+    
+    obj1.start();
+    try {
+      Thread.sleep(2);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+     
+    
+    
+      obj2.start();
+      
+
+    
+  }
+}
